07b613929262662fc0c87189ef97bf1a71103d38
Alinch96 <gusak.alina.9e1@gmail.com>
2024-06-24 10:13:58 +0300

    Finished hw

---
+++ src/main.js
@@ -0,0 +1,113 @@
+import iziToast from 'izitoast';
+import {
+  EVENT_TYPE,
+  MESSAGES,
+  MESSAGES_BG_COLORS,
+  showInfoMessage,
+  refs,
+} from './js/helpers';
+
+import { getGalleryData } from './js/pixabay-api';
+import { fetchGallery } from './js/render-functions';
+
+const { form, gallery, loadMoreBtn } = refs;
+
+let queryString = '';
+let currentPage = 1;
+let evtType = '';
+
+const IMAGE_MAX_COUNT = 15;
+
+form.addEventListener('submit', async e => {
+  e.preventDefault();
+  loadMoreBtn.classList.remove('visible');
+
+  gallery.innerHTML = '';
+  currentPage = 1;
+
+  const search = e.target.search.value.trim();
+
+  evtType = EVENT_TYPE.submit;
+
+  iziToast.destroy();
+
+  queryString = search;
+
+  if (!search) {
+    showInfoMessage(MESSAGES.info, MESSAGES_BG_COLORS.blue);
+    // gallery.innerHTML = '';
+    return;
+  }
+  try {
+    await renderGallery(queryString, currentPage);
+  } catch (error) {
+    console.log('submit', error);
+    showInfoMessage(MESSAGES.exception + error, MESSAGES_BG_COLORS.orange);
+  }
+  e.target.reset();
+});
+
+loadMoreBtn.addEventListener('click', async () => {
+  evtType = EVENT_TYPE.click;
+  loadMoreBtn.classList.remove('visible');
+  try {
+    await renderGallery(queryString, currentPage);
+    const liEl = document.querySelector('li');
+    const { height } = liEl.getBoundingClientRect();
+    scrollVertical(height * 3, 0);
+  } catch (error) {
+    console.log('click', error);
+    showInfoMessage(MESSAGES.exception + error, MESSAGES_BG_COLORS.orange);
+  }
+});
+
+async function renderGallery(searchValue, page) {
+  if (evtType === EVENT_TYPE.click) {
+    page += 1;
+    currentPage = page;
+  }
+  try {
+    const galleryData = await getGalleryData(searchValue, page);
+
+    if (validateGalleryData(galleryData)) {
+      const restOfImages = Math.round(galleryData.totalHits / page);
+      fetchGallery(galleryData);
+      showHideBtn(restOfImages);
+    }
+  } catch (error) {
+    console.log('renderGallery', error);
+    showInfoMessage(MESSAGES.exception + error, MESSAGES_BG_COLORS.orange);
+  }
+  removeLoader();
+}
+
+function scrollVertical(x = 0, y = 0) {
+  window.scrollBy({ top: x, left: y, behavior: 'smooth' });
+}
+
+function removeLoader() {
+  const loaderWrapper = document.querySelector('.loader-wrapper');
+  loaderWrapper.remove();
+}
+
+function validateGalleryData(galleryData) {
+  if (!galleryData) {
+    // gallery.innerHTML = '';
+    return false;
+  } else if (galleryData.totalHits === 0) {
+    showInfoMessage(MESSAGES.warning, MESSAGES_BG_COLORS.red);
+    // gallery.innerHTML = '';
+    return false;
+  } else {
+    return true;
+  }
+}
+
+function showHideBtn(imagesCount) {
+  if (imagesCount <= IMAGE_MAX_COUNT) {
+    loadMoreBtn.classList.remove('visible');
+    showInfoMessage(MESSAGES.endOfSearch, MESSAGES_BG_COLORS.blue);
+    return;
+  }
+  loadMoreBtn.classList.add('visible');
+}
